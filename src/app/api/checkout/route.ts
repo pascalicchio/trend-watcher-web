1698addf6ec40a796f2eba0d03b5c085adc424f8
root <root@srv1313547.hstgr.cloud>
2026-02-04 11:18:28 -0500

    add get

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -1,5 +1,11 @@
-export async function POST() {
+export async function GET() {
   return new Response('{"test":"ok"}', {
+    headers: { 'Content-Type': 'application/json' },
+  });
+}
+
+export async function POST() {
+  return new Response('{"test":"post"}', {
     headers: { 'Content-Type': 'application/json' },
   });
 }
